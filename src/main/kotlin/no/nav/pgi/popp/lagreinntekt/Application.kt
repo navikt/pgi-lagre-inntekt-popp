a432e187610a18a50b58e65e9c525f3fcf6f6b46
paalfb <paalfb@yahoo.se>
2020-10-16 13:42:09 +0200

    Fix. Kafka Consumer should not run on Netty Server.

--- src/main/kotlin/no/nav/pgi/popp/lagreinntekt/Application.kt
+++ src/main/kotlin/no/nav/pgi/popp/lagreinntekt/Application.kt
@@ -1,42 +1,45 @@
 package no.nav.pgi.popp.lagreinntekt
 
-import io.ktor.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import no.nav.pensjon.samhandling.liveness.isAlive
 import no.nav.pensjon.samhandling.liveness.isReady
 import no.nav.pensjon.samhandling.metrics.metrics
-import java.lang.Exception
 
 
 fun main() {
-    createApplication().start(wait = false)
+    val application = Application(KafkaConfig())
+    application.connectAndConsumeFromKafka()
 }
 
-internal fun createApplication(serverPort: Int = 8080, kafkaConfig: KafkaConfig = KafkaConfig()) =
-        embeddedServer(Netty, createApplicationEnvironment(serverPort, kafkaConfig))
+internal class Application(private val kafkaConfig: KafkaConfig = KafkaConfig()) {
 
-private fun createApplicationEnvironment(serverPort: Int, kafkaConfig: KafkaConfig) =
-        applicationEngineEnvironment {
-            connector { port = serverPort }
-            module {
-                isAlive()
-                isReady()
-                metrics()
-                //connectAndConsumeFromKafka(kafkaConfig)
+    init {
+        val server = embeddedServer(Netty, createApplicationEnvironment())
+        server.start(wait = false)
+    }
+
+    private fun createApplicationEnvironment(serverPort: Int = 8080) =
+            applicationEngineEnvironment {
+                connector { port = serverPort }
+                module {
+                    isAlive()
+                    isReady()
+                    metrics()
+                }
             }
-        }
 
-internal fun Application.connectAndConsumeFromKafka(kafkaConfig: KafkaConfig) {
-    val consumer = PensjonsgivendeInntektConsumer(kafkaConfig)
-    while (true) {
-        try {
-            val inntekter = consumer.getInntekter()
-            println("Inntekter fetched: ${inntekter.size}")
-            Thread.sleep(1000)
-        } catch (e: Throwable) {
-            println(e.message)
-            e.printStackTrace()
+    internal fun connectAndConsumeFromKafka() {
+        val consumer = PensjonsgivendeInntektConsumer(kafkaConfig)
+        while (true) {
+            try {
+                val inntekter = consumer.getInntekter()
+                println("Inntekter fetched: ${inntekter.size}")
+                Thread.sleep(1000)
+            } catch (e: Throwable) {
+                println(e.message)
+                e.printStackTrace()
+            }
         }
     }
 }
