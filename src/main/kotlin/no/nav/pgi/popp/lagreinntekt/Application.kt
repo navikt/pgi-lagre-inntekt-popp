ef63ae61a2e95d991d11deb0144d7aa3e6c46568
Marius Eriksen <marius.eriksen@nav.no>
2020-11-18 10:42:16 +0100

    Remove unused logger

--- src/main/kotlin/no/nav/pgi/popp/lagreinntekt/Application.kt
+++ src/main/kotlin/no/nav/pgi/popp/lagreinntekt/Application.kt
@@ -2,7 +2,6 @@ package no.nav.pgi.popp.lagreinntekt
 
 import no.nav.pensjon.samhandling.naisserver.naisServer
 import no.nav.pgi.popp.lagreinntekt.kafka.KafkaConfig
-import org.slf4j.LoggerFactory
 
 fun main() {
     val application = Application()
@@ -27,9 +26,5 @@ internal class Application(kafkaConfig: KafkaConfig = KafkaConfig(), env: Map<St
 
     internal fun stop() {
         naisServer.stop(0, 0)
-    }
-
-    companion object {
-        private val log = LoggerFactory.getLogger(Application::class.java)
     }
 }
