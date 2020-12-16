5c348f281dae187c321ea91b5f889d6629c9b540
Marius Eriksen <marius.eriksen@nav.no>
2020-12-16 13:50:06 +0100

    Add caching of aad tokens

--- src/main/kotlin/no/nav/pgi/popp/lagreinntekt/popp/token/AadTokenClient.kt
+++ src/main/kotlin/no/nav/pgi/popp/lagreinntekt/popp/token/AadTokenClient.kt
@@ -4,8 +4,6 @@ import com.microsoft.aad.msal4j.ClientCredentialFactory
 import com.microsoft.aad.msal4j.ClientCredentialParameters
 import com.microsoft.aad.msal4j.ConfidentialClientApplication
 import no.nav.pensjon.samhandling.env.getVal
-import no.nav.pgi.popp.lagreinntekt.popp.PoppClient
-import no.nav.pgi.popp.lagreinntekt.popp.PoppClient.*
 import no.nav.pgi.popp.lagreinntekt.popp.PoppClient.TokenProvider
 import java.time.LocalDateTime
 import java.time.ZoneId
@@ -15,28 +13,35 @@ internal class AadTokenClient(environment: Map<String, String>) : TokenProvider 
     private val clientPassword = environment.getVal(CLIENT_PASSWORD)
     private val targetApiId = environment.getVal(TARGET_API_ID)
     private val authorityUrl = environment.getVal(WELL_KNOWN_URL)
-
     private val scopes = setOf("api://$targetApiId/.default")
     private val clientSecret = ClientCredentialFactory.createFromSecret(clientPassword)
     private val confidentialClientApplication = createConfidentialClientApplication()
+    private var cachedToken: AadToken? = null
 
+    override fun getToken(): AadToken = refreshToken(this.cachedToken)
 
-    override fun getToken(): AadToken {
+    private fun refreshToken(aadToken: AadToken?) =
+        if (aadToken == null || tokenExpiresWithinTwoMinutes(aadToken)) getAadToken() else aadToken
+
+    private fun tokenExpiresWithinTwoMinutes(aadToken: AadToken) =
+        aadToken.expires.isBefore(LocalDateTime.now().minusMinutes(2))
+
+    private fun getAadToken(): AadToken {
         val clientCredentialParameters = ClientCredentialParameters.builder(scopes).build()
         val authenticationResult = confidentialClientApplication.acquireToken(clientCredentialParameters).get()
 
         return AadToken(
-                authenticationResult.accessToken(),
-                authenticationResult.expiresOnDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
+            authenticationResult.accessToken(),
+            authenticationResult.expiresOnDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDateTime()
         ).apply { println("Token expires at date: $expires") }
     }
 
     private fun createConfidentialClientApplication() =
-            ConfidentialClientApplication.builder(clientId, clientSecret).authority(authorityUrl).build()
+        ConfidentialClientApplication.builder(clientId, clientSecret).authority(authorityUrl).build()
 
     internal data class AadToken(
-            val accessToken: String,
-            val expires: LocalDateTime
+        val accessToken: String,
+        val expires: LocalDateTime
     )
 
     private companion object EnvironmentKeys {
@@ -45,10 +50,4 @@ internal class AadTokenClient(environment: Map<String, String>) : TokenProvider 
         private const val CLIENT_PASSWORD = "AZURE_APP_CLIENT_SECRET"
         private const val TARGET_API_ID = "AZURE_APP_TARGET_API_ID"
     }
-
-    internal fun refreshToken(aadToken: AadToken) =
-            if (tokenExpiresWithinTwoMinutes(aadToken)) getToken() else aadToken
-
-    private fun tokenExpiresWithinTwoMinutes(aadToken: AadToken) =
-            aadToken.expires.isBefore(LocalDateTime.now().minusMinutes(2))
 }
