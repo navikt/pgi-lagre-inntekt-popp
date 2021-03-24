79cad7eca2278b0d6e8b758adfe9b5b43f81b125
Oyvind Buer <oyvindbuer@gmail.com>
2021-03-24 15:11:03 +0100

    changes label from CamelCase to Underscore for pgiPoppResponseCounter

--- src/main/kotlin/no/nav/pgi/popp/lagreinntekt/LagreInntektPopp.kt
+++ src/main/kotlin/no/nav/pgi/popp/lagreinntekt/LagreInntektPopp.kt
@@ -78,12 +78,12 @@ internal class LagreInntektPopp(
     }
 
     private fun logPidValidationFailed(response: HttpResponse<String>, pgi: PensjonsgivendeInntekt) {
-        pgiPoppRespnseCounter.labels("${response.statusCode()}_PidValidation").inc()
+        pgiPoppRespnseCounter.labels("${response.statusCode()}_Pid_Validation").inc()
         LOG.warn(("Failed when adding to POPP. Inntekt will be descarded. Pid did not validate ${response.logString()}. For pgi: $pgi").maskFnr())
     }
 
     private fun logInntektAarValidationFailed(response: HttpResponse<String>, pgi: PensjonsgivendeInntekt?) {
-        pgiPoppRespnseCounter.labels("${response.statusCode()}_InnntektAarValidation").inc()
+        pgiPoppRespnseCounter.labels("${response.statusCode()}_InnntektAar_Validation").inc()
         LOG.warn(("Failed when adding to POPP. Inntekt will be descarded. Inntektaar is not valid for pgi. ${response.logString()}. For pgi: $pgi ").maskFnr())
     }
 
